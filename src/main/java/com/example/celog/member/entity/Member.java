ab1d3432039cfe91d7696e0b6b610ae1b689cf50
micheal1woo <53082557+micheal1woo@users.noreply.github.com>
2023-03-03 15:23:53 +0900

    feat : 인증/인가 구현 (refreshtoken 포함)

--- src/main/java/com/example/celog/member/entity/Member.java
+++ src/main/java/com/example/celog/member/entity/Member.java
@@ -1,2 +1,38 @@
-package com.example.celog.member.entity;public class Member {
+package com.example.celog.member.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+
+
+
+    @Builder
+    public Member(String nickname, String password, String email) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+
+    }
+
+
+
 }
