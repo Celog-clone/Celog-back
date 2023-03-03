50d77acb24c30adbfd49a8a2ddcc13185eadff5b
kingaser <kignaser@naver.com>
2023-03-03 10:52:29 +0900

    feat:기ì spring ´ 설정

---
+++ src/test/java/com/example/celog/CelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.celog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
