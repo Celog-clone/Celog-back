5475330abdf64e96bae51e1a09f7ab5a1af60157
kingaser <kignaser@naver.com>
2023-03-04 17:36:04 +0900

    feat: post 기능 merge

--- src/main/java/com/example/celog/post/entity/Post.java
+++ src/main/java/com/example/celog/post/entity/Post.java
@@ -1,58 +1,7 @@
 package com.example.celog.post.entity;
 
-import com.example.celog.comment.entity.Comment;
-import com.example.celog.member.Entity.Member;
-import com.example.celog.post.dto.PostRequestDto;
-import com.example.celog.superclass.Timestamped;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-public class Post extends Timestamped{
-    @Id
-    @Column(name = "post_id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Post {
 
-    @Column(nullable = false)
-    private String title;
-
-    @Column(nullable = false)
-    private String contents;
-
-    @Column(nullable = false)
-    private String image;
-
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    @OneToMany(mappedBy = "post")
-    private List<Comment> comment = new ArrayList<>();
-
-    @Builder
-    private Post(String title, String contents, String image, Member member) {
-        this.title = title;
-        this.contents = contents;
-        this.image = image;
-        this.member = member;
-    }
-
-    public static Post of(PostRequestDto postRequestDto, Member member) {
-        Post post = Post.builder().title(postRequestDto.getTitle()).contents(postRequestDto.getContents()).image(postRequestDto.getImage()).member(member).build();
-        return post;
-    }
-    public void update(String title, String contents, String image) {
-        this.title = title;
-        this.contents = contents;
-        this.image = image;
-    }
 }
