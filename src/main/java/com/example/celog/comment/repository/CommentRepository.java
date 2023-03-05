aa5bc748b06f956275a012f4887e56ba46983596
kingaser <kignaser@naver.com>
2023-03-05 11:01:05 +0900

    feat: CommentRepository 댓글 정렬 기준 추가(필요없으면 추후 삭제 예정) #18

--- src/main/java/com/example/celog/comment/repository/CommentRepository.java
+++ src/main/java/com/example/celog/comment/repository/CommentRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllById(Long id);
-//    List<Comment> findAllByPostIdAndOrderByModifiedAt(Long id);
+    List<Comment> findAllByPostIdOrderByModifiedAtDesc(Long id);
 
 }
