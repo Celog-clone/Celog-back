8b957e57f61677b08d4bf6dd16330686e265c712
kingaser <kignaser@naver.com>
2023-03-05 09:53:52 +0900

    feat: CommentController/Service 반환 타입 변경, 중복 예외 처리 리펙토링 #18

--- src/main/java/com/example/celog/comment/controller/CommentController.java
+++ src/main/java/com/example/celog/comment/controller/CommentController.java
@@ -3,15 +3,14 @@ package com.example.celog.comment.controller;
 import com.example.celog.comment.dto.CommentRequestDto;
 import com.example.celog.comment.dto.CommentResponseDto;
 import com.example.celog.comment.service.CommentService;
+import com.example.celog.common.ApiResponseDto;
 import com.example.celog.common.SuccessResponse;
 import com.example.celog.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,20 +19,15 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    @GetMapping("/comments/{id}")
-    public ResponseEntity<List<CommentResponseDto>> commentList(@PathVariable Long id) {
-        return commentService.listComment(id);
-    }
-
     @PostMapping("/posts/{id}/comments")
-    public ResponseEntity<CommentResponseDto> commentSave(@PathVariable Long id,
+    public ApiResponseDto<CommentResponseDto> commentSave(@PathVariable Long id,
                                                           @Valid @RequestBody CommentRequestDto commentRequestDto,
                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.saveComment(id, commentRequestDto, userDetails.getUser());
     }
 
     @PutMapping("/posts/{id}/comments/{comment_id}")
-    public ResponseEntity<CommentResponseDto> commentModify(@PathVariable Long id,
+    public ApiResponseDto<CommentResponseDto> commentModify(@PathVariable Long id,
                                                             @Valid @PathVariable Long comment_id,
                                                             @RequestBody CommentRequestDto commentRequestDto,
                                                             @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -41,7 +35,7 @@ public class CommentController {
     }
 
     @DeleteMapping("/posts/{id}/comments/{comment_id}")
-    public ResponseEntity<SuccessResponse> commentRemove(@PathVariable Long id,
+    public ApiResponseDto<SuccessResponse> commentRemove(@PathVariable Long id,
                                                          @PathVariable Long comment_id,
                                                          @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.removeComment(id, comment_id, userDetails.getUser());
