86cbe3daa5766f79b31b92cd714e0bc562efd67a
kingaser <kignaser@naver.com>
2023-03-06 12:11:45 +0900

    Refactor: UserDetailsImpl -> Member로 수정 #18

--- src/main/java/com/example/celog/like/service/LikeService.java
+++ src/main/java/com/example/celog/like/service/LikeService.java
@@ -2,12 +2,12 @@ package com.example.celog.like.service;
 
 import com.example.celog.common.ApiResponseDto;
 import com.example.celog.common.ResponseUtils;
-import com.example.celog.common.SuccessResponse;
+import com.example.celog.like.dto.LikeResponseDto;
 import com.example.celog.like.entity.Like;
 import com.example.celog.like.repository.LikeRepository;
+import com.example.celog.member.Entity.Member;
 import com.example.celog.post.entity.Post;
 import com.example.celog.post.repository.PostRepository;
-import com.example.celog.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -25,19 +25,23 @@ public class LikeService {
     private final LikeRepository likeRepository;
     private final PostRepository postRepository;
 
-    public ApiResponseDto<SuccessResponse> saveLike(Long id, UserDetailsImpl userDetails) {
+    public ApiResponseDto<LikeResponseDto> saveLike(Long id, Member member) {
         Post post = postRepository.findById(id).orElseThrow(
                 () -> new NullPointerException(NOT_EXIST_POST.getMsg())
         );
 
-        Optional<Like> found = likeRepository.findByPostAndMember(post, userDetails.getMember());
+        Optional<Like> found = likeRepository.findByPostAndMember(post, member);
         if (found.isEmpty()) {
-            likeRepository.save(Like.of(post, userDetails.getMember()));
-            return ResponseUtils.ok(SuccessResponse.of(HttpStatus.OK, "좋아요 성공"));
+            Like like = likeRepository.save(Like.of(post, member));
+            return ResponseUtils.ok(LikeResponseDto.from(like, true, HttpStatus.OK.value(), "좋아요 성공"));
         } else {
+            Like like = likeRepository.findByMember(member).orElseThrow(
+                    () -> new NullPointerException("해당 게시글에 좋아요 내역이 없습니다.")
+            );
             likeRepository.delete(found.get());
             likeRepository.flush();
-            return ResponseUtils.ok(SuccessResponse.of(HttpStatus.OK, "좋아요 취소"));
+            return ResponseUtils.ok(LikeResponseDto.from(like, false, HttpStatus.OK.value(), "좋아요 취소"));
         }
     }
 }
+
